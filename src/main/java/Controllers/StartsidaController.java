3d92d81c7a428283c4c2c744b3eec2a8f17bf34d
Jepsson <davjep-0@student.ltu.se>
2021-05-02 16:28:59 +0200

    Adding javafx to project. Created first .fxml file with Controller //DJ

---
+++ src/main/java/Controllers/StartsidaController.java
@@ -0,0 +1,33 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartsidaController {
+
+    @FXML
+    private Button loggaInKnapp;
+
+    @FXML
+    private Button sökKnapp;
+
+    @FXML
+    private Button registreraKnapp;
+
+    @FXML
+    void tryckLoggaIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tryckRegistreraKnapp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tryckSökKnapp(ActionEvent event) {
+
+    }
+
+}
