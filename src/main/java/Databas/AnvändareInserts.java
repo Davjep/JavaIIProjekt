710aef040007348a563f40d3db511090e9ea6923
Jepsson <davjep-0@student.ltu.se>
2021-05-03 08:36:34 +0200

    Skapat sql connection klass med metod v2//DJ

--- src/main/java/Databas/AnvändareInserts.java
+++ src/main/java/Databas/AnvändareInserts.java
@@ -1,27 +1,13 @@
 package Databas;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class AnvändareInserts {
-    private String url = "jdbc:mysql://localhost/";
-    private static final String USERNAME = "Jepsson";
-    private static final String PASSWORD = "Ol@f4lif3";
-    private PreparedStatement selectUser;
-    private Connection connection;
-    private String databaseName;
 
-    public Connection getConnection() {
-        try {
-            databaseName = "biblioteket";
-            url += databaseName;
-            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
-        } catch (SQLException sqlException) {
-            sqlException.printStackTrace();
-            System.exit(1);
-        }
-        return connection;
+    public String sqlInsertAnvändare(String användarId, String förNamn, String efterNamn, String telefon, String adress,
+                                     String email, String personNr, int ålder, String typ, int antalLåneObjekt) {
+
+        String sqlInsertQuery = "INSERT INTO användare (AnvändarId, FörNamn, EfterNamn, Telefon, Adress, Email, PersonNr, Ålder, Typ, AntalLåneObjekt)" +
+                "VALUES ('" + användarId + "','" + förNamn + "','" + efterNamn + "','" + telefon + "','" + adress + "','" + email + "','" +
+                personNr + "'," + ålder + ",'" + typ + "', " + antalLåneObjekt + ")";
+        return sqlInsertQuery;
     }
 }
